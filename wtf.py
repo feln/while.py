36367e3a70cc9fa9d0e7ffef7752b748f0bcf1c9
feln <60304442+feln@users.noreply.github.com>
2020-02-17 11:45:05 +0800

    Initial commit

---
+++ wtf.py
@@ -0,0 +1,11 @@
+age=37
+if age<30:
+    print("whats point")
+    if age<27:
+       print("87")
+    if age<24:
+       print("kid")
+    if age<21:
+       print("you out")
+else:
+    print("OK We can talk!")
