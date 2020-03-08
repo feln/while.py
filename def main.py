d0c3f551eda3243cf5dd35de696f3127ec1107e6
feln <60304442+feln@users.noreply.github.com>
2020-03-08 16:13:53 +0800

    Update def main.py

--- def main.py
+++ def main.py
@@ -1,5 +1,7 @@
 def main():
     #使用者輸入任意文字
-    content = input('請隨意輸入一段文字')
+    content = input('請隨意輸入一段文字:')
     #呼叫dispaly函數
     dispaly(content)
+def dispaly(str):
+    print('你輸入的內容是:'+str)
